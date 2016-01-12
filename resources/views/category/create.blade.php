3808a36ebac4f6245adcb22ce209d05ab935a270
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-12 18:48:57 +0600

    Category creation form put into partials folder for enabling reuse

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -7,15 +7,7 @@
 @section('content')
     <h1>New Category</h1>
     {!! Form::open(array('route' => 'categories.store'))!!}
-        <div class="form-group">
-            {!! Form::label('title','Title') !!}
-            {!! Form::text('title',null,array('class'=>'form-control','placeholder'=>'Give A Title')) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('parent','Parent') !!}
-            {!! Form::select('parent', $categories,null,array('class'=>'form-control')); !!}
-        </div>
-    {!! Form::submit('Add Category',array('class'=>'btn btn-large btn-primary')) !!}
+        @include('category.partials.form',['submitBtnText'=>'Add Category'])
     {!! Form::close() !!}
 
 @stop
---
+++ resources/views/category/partials/form.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group">
+    {!! Form::label('title','Title') !!}
+    {!! Form::text('title',null,array('class'=>'form-control','placeholder'=>'Give A Title')) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('parent','Parent') !!}
+    {!! Form::select('parent', $categories,null,array('class'=>'form-control')); !!}
+</div>
+{!! Form::submit($submitBtnText,array('class'=>'btn btn-large btn-primary')) !!}
+<a class="btn btn-large btn-default" href="{{route('categories.index')}}">Cancel</a>
