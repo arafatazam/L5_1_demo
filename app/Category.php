40d70ba1d40281f41950d7dda0b98e477e6fb49a
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-12 08:42:10 +0600

    Category model added

---
+++ app/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['title'];
+
+    public function parent()
+    {
+        return $this->hasOne('App\Category','parent_id');
+    }
+}
