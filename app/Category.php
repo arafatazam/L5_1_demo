f333ac68e74441d460e6ccbbc7c923e460457711
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-12 23:49:17 +0600

    Category model relationship fixed

--- app/Category.php
+++ app/Category.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['title'];
+    protected $fillable = ['title','parent_id'];
 
     public function parent()
     {
-        return $this->hasOne('App\Category','parent_id');
+        return $this->belongsTo('App\Category','parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Category','parent_id');
     }
 
     public function products()
