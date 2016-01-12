8dc8a403eec80883e9e921039baa2cd2761fa8f1
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-12 18:42:18 +0600

    Action button anchors set in category.index view

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <h1>Categories</h1>
-    <a class="btn btn-large btn-success" href="#">Add New</a>
+    <a class="btn btn-large btn-success" href="{{route('categories.create')}}">Add New</a>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -21,9 +21,8 @@
                 <td>{{$category['title']}}</td>
                 <td>{{$category['parent']['title'] or 'N/A'}}</td>
                 <td>
-                    <a class="btn btn-warning" href="#">View</a>
-                    <a class="btn btn-success" href="#">Edit</a>
-                    <a class="btn btn-danger" href="#">Delete</a>
+                    <a class="btn btn-primary" href="{{route('categories.edit',[$category['id']])}}">Edit</a>
+                    <a class="btn btn-danger" href="{{route('categories.destroy',[$category['id']])}}">Delete</a>
                 </td>
             </tr>
             @endforeach
