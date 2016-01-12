bdb200773155312efa49ee17fb992ca766cec2b4
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-12 09:21:15 +0600

    Many to Many relationship established between Category and Product

--- app/Category.php
+++ app/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     {
         return $this->hasOne('App\Category','parent_id');
     }
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Product');
+    }
 }
