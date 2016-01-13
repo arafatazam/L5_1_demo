5de459ea81b4e0ea568c0615eb287e7e9b7c4fa2
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-14 02:41:14 +0600

    Foreign key constraints added to category_product pivot table

---
+++ database/migrations/2016_01_13_203627_add_product_foreign_key_constraints_to_category_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProductForeignKeyConstraintsToCategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->foreign('product_id')
+                ->references('id')->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->dropForeign('products_product_id_foreign');
+        });
+    }
+}
