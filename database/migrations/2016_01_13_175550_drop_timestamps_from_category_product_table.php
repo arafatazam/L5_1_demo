480e9df2e5132c8be897622b511b6597bdede5f6
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-14 00:07:31 +0600

    Timestamps removed from category_product table for avoiding not null violation

---
+++ database/migrations/2016_01_13_175550_drop_timestamps_from_category_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropTimestampsFromCategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('category_product', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+}
