7f1a0f7645e1e2880107218b5bd8e8d2613c6572
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-13 09:20:49 +0600

    scripts section added to app.blade.php for extendability

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,12 @@
         <div class="container">
             @yield('content')
         </div>
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+        @section('scripts')
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        @show
+
     </body>
 </html>
