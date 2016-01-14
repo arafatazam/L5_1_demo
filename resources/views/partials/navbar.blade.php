a6f600df629650e5ea49b4dc2cbb899894f0d425
Muhammad A Azam <azzy.bu@gmail.com>
2016-01-15 00:43:44 +0600

    Access specific nav bar made ready

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -14,7 +14,9 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+                @can('view')
                 <li><a href="{{route('products.index')}}">Products</a></li>
+                @endcan
                 @can('administer')
                 <li><a href="{{route('categories.index')}}">Categories</a></li>
                 @endcan
@@ -23,10 +25,13 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ auth()->check()? auth()->user()->name : 'Session' }} <span class="caret"></span></a>
                     <ul class="dropdown-menu">
+                        @cannot('view')
                         <li><a href="{{route('auth.get-register')}}">Sign Up</a></li>
                         <li><a href="{{route('auth.get-login')}}">Sign In</a></li>
-                        <li role="separator" class="divider"></li>
+                        @endcannot
+                        @can('view')
                         <li><a href="{{route('auth.logout')}}">Sign out</a></li>
+                        @endcan
                     </ul>
                 </li>
             </ul>
